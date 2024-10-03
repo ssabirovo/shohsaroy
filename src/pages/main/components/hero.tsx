c1c496bb67755e452652bda1e3b44712c4693034
ssabirovo <muhiddinsobirov957@gmail.com>
2024-10-03 17:27:15 +0500

    perf: added notification

--- src/pages/main/components/hero.tsx
+++ src/pages/main/components/hero.tsx
@@ -1,87 +1,137 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
+import { CheckCircleFilled, CloseCircleFilled } from "@ant-design/icons";
 import { useTranslation } from "react-i18next";
+import { notification } from "antd";
+import "./hero.css";
+
+type typeOpenNotification = {
+  message: string;
+  description: string;
+  isSuccess: boolean;
+};
 
 const Hero: React.FC = () => {
   const { t } = useTranslation();
+  const [api, contextHolder] = notification.useNotification();
+  const [isPosting, setIsPosting] = useState(false);
+
+  const openNotification = ({
+    message,
+    description,
+    isSuccess,
+  }: typeOpenNotification): void => {
+    api.open({
+      message: message,
+      description: description,
+      duration: 5000,
+      icon: isSuccess ? (
+        <CheckCircleFilled className="text-main" />
+      ) : (
+        <CloseCircleFilled className="text-red-500" />
+      ),
+    });
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // Always good practice to prevent the default form submission behavior
+    setIsPosting(true);
     const formData = new FormData(e.target as HTMLFormElement);
     const payload = Object.fromEntries(formData);
 
-    axios.post(
-      "https://service.app.uysot.uz/v1/external-source",
-      {
-        esType: "WEBSITE",
-        message: "https://shohsaroy.uz/#rec664088122",
-        name: payload?.name,
-        phoneNumber: payload?.phone,
-      },
-      {
-        headers: {
-          "X-Auth":
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5MyIsImV4cCI6Njc5OTgyMTg0OX0.-9lTMAIshpDjDw0TU0Z6itGyNrJ4Afu7Yvupir8tS3w",
+    axios
+      .post(
+        "https://service.app.uysot.uz/v1/external-source",
+        {
+          esType: "WEBSITE",
+          message: "https://shohsaroy.uz/#rec664088122",
+          name: payload?.name,
+          phoneNumber: payload?.phone,
         },
-      },
-    );
+        {
+          headers: {
+            "X-Auth":
+              "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5MyIsImV4cCI6Njc5OTgyMTg0OX0.-9lTMAIshpDjDw0TU0Z6itGyNrJ4Afu7Yvupir8tS3w",
+          },
+        },
+      )
+      .then(() => {
+        openNotification({
+          message: t("notification.successTitle"),
+          description: t("notification.successDescription"),
+          isSuccess: true,
+        });
+        setIsPosting(false);
+      })
+      .catch(() => {
+        openNotification({
+          message: t("notification.errorTitle"),
+          description: t("notification.errorDescription"),
+          isSuccess: false,
+        });
+        setIsPosting(false);
+      });
   };
 
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-[url('/assets/hero-bg-2.png')] bg-cover bg-center max-lg:items-end max-sm:h-[135vh]">
-      <div className="flex h-max w-full max-w-1200 items-start justify-between gap-14 px-3 py-4 max-lg:flex-col max-lg:items-center max-sm:h-full max-sm:gap-0 max-sm:p-0">
-        <div className="text-white max-lg:text-center max-sm:hidden">
-          <h1 className="text-5xl max-md:text-3xl">{t("hero.title")}</h1>
-          <p className="text-2xl">{t("hero.description")}</p>
-        </div>
-        <div className="hidden h-full w-full flex-col justify-end bg-main bg-[url('/assets/hero-bg-mobile.png')] bg-cover text-white max-sm:flex">
-          <div className="flex h-[50%] flex-col justify-end bg-black-gradient px-3 py-10">
-            <h1 className="text-3xl">{t("hero.title")}</h1>
-            <p className="text-base">{t("hero.description")}</p>
+    <>
+      {contextHolder}
+      <div className="flex h-screen w-full items-center justify-center bg-[url('/assets/hero-bg-2.png')] bg-cover bg-center max-lg:items-end max-sm:h-[135vh]">
+        <div className="flex h-max w-full max-w-1200 items-start justify-between gap-14 px-3 py-4 max-lg:flex-col max-lg:items-center max-sm:h-full max-sm:gap-0 max-sm:p-0">
+          <div className="text-white max-lg:text-center max-sm:hidden">
+            <h1 className="text-5xl max-md:text-3xl">{t("hero.title")}</h1>
+            <p className="text-2xl">{t("hero.description")}</p>
+          </div>
+          <div className="hidden h-full w-full flex-col justify-end bg-main bg-[url('/assets/hero-bg-mobile.png')] bg-cover text-white max-sm:flex">
+            <div className="flex h-[50%] flex-col justify-end bg-black-gradient px-3 py-10">
+              <h1 className="text-3xl">{t("hero.title")}</h1>
+              <p className="text-base">{t("hero.description")}</p>
+            </div>
+          </div>
+
+          <div className="flex w-[35%] flex-col gap-5 rounded-2xl bg-main p-9 text-white max-md:w-[65%] max-sm:w-full max-sm:rounded-none max-sm:bg-[url('/assets/form-bg.svg')]">
+            <h1 className="max-md:text-1xl text-xl">{t("hero.form.title")}</h1>
+            <form
+              onSubmit={handleSubmit}
+              action=""
+              className="flex flex-col gap-2"
+            >
+              <div className="flex flex-col gap-1">
+                <label className="text-xl" htmlFor="name">
+                  {t("hero.form.nameLabel")}
+                </label>
+                <input
+                  name="name"
+                  className="rounded p-2 text-main outline-main"
+                  type="text"
+                  id="name"
+                  placeholder={t("hero.form.namePlaceholder")}
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <label className="text-xl" htmlFor="phone">
+                  {t("hero.form.phoneLabel")}
+                </label>
+                <input
+                  name="phone"
+                  className="rounded p-2 text-main outline-main"
+                  type="text"
+                  id="phone"
+                  placeholder={t("hero.form.phonePlaceholder")}
+                />
+              </div>
+              <button
+                disabled={isPosting}
+                className="mt-6 rounded border border-white bg-white p-2 text-main transition-all hover:bg-main hover:text-white"
+                type="submit"
+              >
+                {t("hero.form.submit")}
+              </button>
+            </form>
           </div>
         </div>
-
-        <div className="flex w-[35%] flex-col gap-5 rounded-2xl bg-main p-9 text-white max-md:w-[65%] max-sm:w-full max-sm:rounded-none max-sm:bg-[url('/assets/form-bg.svg')]">
-          <h1 className="max-md:text-1xl text-xl">{t("hero.form.title")}</h1>
-          <form
-            onSubmit={handleSubmit}
-            action=""
-            className="flex flex-col gap-2"
-          >
-            <div className="flex flex-col gap-1">
-              <label className="text-xl" htmlFor="name">
-                {t("hero.form.nameLabel")}
-              </label>
-              <input
-                name="name"
-                className="rounded p-2 text-main outline-main"
-                type="text"
-                id="name"
-                placeholder={t("hero.form.namePlaceholder")}
-              />
-            </div>
-            <div className="flex flex-col gap-1">
-              <label className="text-xl" htmlFor="phone">
-                {t("hero.form.phoneLabel")}
-              </label>
-              <input
-                name="phone"
-                className="rounded p-2 text-main outline-main"
-                type="text"
-                id="phone"
-                placeholder={t("hero.form.phonePlaceholder")}
-              />
-            </div>
-            <button
-              className="mt-6 rounded border border-white bg-white p-2 text-main transition-all hover:bg-main hover:text-white"
-              type="submit"
-            >
-              {t("hero.form.submit")}
-            </button>
-          </form>
-        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/main/components/phone.tsx
+++ src/pages/main/components/phone.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Phone: React.FC = () => (
   <a
     href="tel:+998555049999"
-    className="fixed bottom-10 right-10 z-10 flex h-14 w-14 items-center justify-center rounded-2xl bg-phone"
+    className="fixed bottom-10 right-3 z-10 flex h-14 w-14 items-center justify-center rounded-2xl bg-phone"
   >
     <img width="24px" src="/assets/phone.svg" alt="" />
   </a>
